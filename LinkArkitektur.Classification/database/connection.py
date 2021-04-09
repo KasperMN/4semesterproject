7e19f9b3e7a7d45f2a576414be0d82b613564598
Kasper Nielsen <Nielsen1994@hotmail.com>
2021-04-09 09:40:23 +0200

    Changes

--- LinkArkitektur.Classification/database/connection.py
+++ LinkArkitektur.Classification/database/connection.py
@@ -5,33 +5,28 @@ import json
 pd.set_option("display.width", 300)
 pd.set_option("display.max_columns", 20)
 
-url = "https://link.speckle.dk/api/streams/grB5WJmsE/objects"
-JSONContent = requests.get(url, verify=False).json()
-content = json.dumps(JSONContent, indent = 4, sort_keys=True)
-df = pd.read_json(content)
 
-index = {'_id': []}
+class ApiConnection:
+    def __init__(self):
+        self.url = "https://link.speckle.dk/api/streams/grB5WJmsE/objects"
+        self.JSONContent = requests.get(self.url, verify=False).json()
+        self.content = json.dumps(self.JSONContent, indent=4, sort_keys=True)
+        self.df = pd.read_json(self.content)
+        self.index = {'_id': []}
+        self.values = {'Assembly_Code': [], 'Area': [], 'Structural': [], 'Volume': [], 'Base Constraint': []}
 
-values = {'Assembly_Code': [],
-          'Area': [],
-          'Structural': [],
-          'Volume': [],
-          'Base Constraint': [],
-          }
+    def connect_database(self):
+        for i, _ in enumerate(self.df['resources']):
+            wall_id = (self.df['resources'][i]['_id'])  # Specific wall id's
+            parameters = (self.df['resources'][i]['properties']['parameters'])  # Regular parameters for the wall
+            type_parameters = (self.df['resources'][i]['properties']['typeParameters'])  # Parameters connected with the type of wall
 
-for i, _ in enumerate(df['resources']):
-    wall_id = (df['resources'][i]['_id'])  # Specific wall id's
-    parameters = (df['resources'][i]['properties']['parameters'])  # Regular parameters for the wall
-    type_parameters = (df['resources'][i]['properties']['typeParameters'])  # Parameters connected with the type of wall
+            ''' Collecting values to dictionaries {}'''
+            self.index['_id'].append(wall_id)  # Wall Identification Number
+            self.values['Assembly_Code'].append(type_parameters['Assembly Code'])  # The target label
+            self.values['Area'].append(parameters['Area'])  # Area in Cubic Meters
+            self.values['Structural'].append(parameters['Structural'])  # Boolean value for structural use
+            self.values['Volume'].append(parameters['Volume'])  # The volume of the Wall
+            self.values['Base Constraint'].append(parameters['Base Constraint'])
 
-    ''' Collecting values to dictionaries {}'''
-    index['_id'].append(wall_id)  # Wall Identification Number
-    values['Assembly_Code'].append(type_parameters['Assembly Code'])  # The target label
-    values['Area'].append(parameters['Area'])  # Area in Cubic Meters
-    values['Structural'].append(parameters['Structural'])  # Boolean value for structural use
-    values['Volume'].append(parameters['Volume'])  # The volume of the Wall
-    values['Base Constraint'].append(parameters['Base Constraint'])
-
-somethinjesperneeds = pd.DataFrame(data=values, index=index['_id'])  # Values as columns, index as indexes
-
-print(somethinjesperneeds)
+        return pd.DataFrame(data=self.values, index=self.index['_id'])  # Values as columns, index as indexes
