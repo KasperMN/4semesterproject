eed764a98455ff18d9fa9930a130786e8910ac0e
JesperMeller <jesper.meller@hotmail.com>
2021-05-31 15:22:44 +0200

    tgrf

--- automated_classifier-git/automated_classifier/machinelearning/classifiers.py
+++ automated_classifier-git/automated_classifier/machinelearning/classifiers.py
@@ -4,11 +4,12 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.svm import SVC
 
 
 class KNeighbors:
     def __init__(self, name):
-        self._params = defaultdict(leaf_size=[1, 2, 3, 5, 10, 20],
+        self._params = defaultdict(leaf_size=[10, 20, 30, 40],
                                    n_neighbors=[1, 2, 3, 5, 10],
                                    weights=["uniform", "distance"],
                                    algorithm=["auto", "ball_tree", "kd_tree", "brute"],
@@ -45,11 +46,9 @@ class KNeighbors:
 
 class GradientBoost:
     def __init__(self, name):
-        self._params = defaultdict(n_estimators=[1, 2, 3, 4, 5, 10, 20],
+        self._params = defaultdict(n_estimators=[5, 10],
                                    learning_rate=[1, 2, 3, 4, 5],
-                                   max_depth=[1, 2, 3, 5],
-                                   max_features=["auto", "sqrt", "log2"],
-                                   max_leaf_nodes=[10, 20])
+                                   max_features=["auto", "sqrt", "log2"])
         self._model = GradientBoostingClassifier()
         self._name = name
 
@@ -81,11 +80,12 @@ class GradientBoost:
 
 class RandomForest:
     def __init__(self, name):
-        self._params = defaultdict(n_estimators=[1, 5, 10, 15, 20, 50, 85, 100],
-                                   min_samples_split=[1, 2, 3, 5, 10],
-                                   max_depth=[1, 2, 3, 5, 10, 20, 40, 50],
-                                   min_samples_leaf=[1, 2, 3, 5, 10],
-                                   class_weight=["balanced", "balanced_subsample"])
+        self._params = defaultdict(n_estimators=[1, 5, 10, 15, 20, 50, 85, 100, 150, 200],
+                                   max_depth=[10, 20, 40, 50, 100],
+                                   class_weight=["balanced", "balanced_subsample"],
+                                   max_features=["auto", "sqrt", "log2"],
+                                   warm_start=[True, False],
+                                   criterion=['gini', 'entropy'])
         self._model = RandomForestClassifier()
         self._name = name
 
@@ -113,3 +113,38 @@ class RandomForest:
         print('\n---> {0}: Found Parameters in {1:.2f} seconds '.format(name, end - start))
         print("Parameters = ", rscv.best_params_)
         return self
+
+class SupportVector:
+    def __init__(self, name):
+        self._params = defaultdict(kernel=['linear', 'poly', 'rbf', 'sigmoid', 'precomputed'],
+                                   gamma=['scale', 'auto'],
+                                   decision_function_shape=['ovo', 'ovr'],
+                                   shrinking=[True, False])
+        self._model = SVC()
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def model(self):
+        return self._model
+
+    @model.setter
+    def model(self, model):
+        self._model = model
+
+    def find_best_estimator(self, training_features, training_labels, name):
+        print("---> {}: Finding Parameters".format(name))
+        start = time.time()
+
+        gscv = GridSearchCV(self._model, self._params, cv=3)
+        gscv.fit(training_features, training_labels.values.ravel())
+        self._model = gscv.best_estimator_
+
+        end = time.time()
+        print('\n---> {0}: Found Parameters in {1:.2f} seconds '.format(name, end - start))
+        print("Parameters = ", gscv.best_params_)
+        return self
+
